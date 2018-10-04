a0c69ded8f62ce6b4c58f551108119ac68cd22e5
Björn Petersen <pheasn@gmail.com>
2018-10-04 18:23:06 +0200

    Use html instead of html2 generator for docs
    The html2 generator looks better, but outputs empty schemas.
    Jesus Christ, the OpenAPI code generator project is a shit show.

--- generateClient.sh
+++ generateClient.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -jar openapi-generator-cli.jar generate -i musicbot.yaml -o gen -c config.json -l java --library=okhttp-gson
+java -jar openapi-generator-cli.jar generate -i musicbot.yaml -o gen -c config.json -g java --library=okhttp-gson
